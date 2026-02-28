a561c84aaff4bacf86f538f00e66d86a4303eca8
vecel <mateusz.karandys@gmail.com>
2026-02-28 17:44:19 +0100

    first model

--- models/combine_features.py
+++ models/combine_features.py
@@ -6,3 +6,32 @@ def combine_features():
     olek_features = pd.read_csv("../features/olek-features.csv")
     combined = pd.merge(olek_features, mateusz_features, on="Area")
     return combined
+
+
+def read_data(path):
+    data = pd.read_csv(path)
+    df = pd.merge(data, combine_features(), on="ID")
+    df = df.drop(
+        columns=["start_date", "end_date", "horizon_lower", "horizon_upper", "Country", "electrical_conductivity"]
+    )
+    target_cols = [
+        "Al",
+        "B",
+        "Ca",
+        "C_organic",
+        "C_total",
+        "Cu",
+        "Fe",
+        "Mg",
+        "Mn",
+        "N",
+        "P",
+        "K",
+        "Na",
+        "S",
+        "Zn",
+    ]
+    other_cols = [col for col in df.columns if col not in target_cols]
+    new_columns = other_cols + target_cols
+    df = df[new_columns]
+    return df
